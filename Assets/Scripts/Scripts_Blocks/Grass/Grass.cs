cc1779180bd39147e8ce7bb0c699d83e2628c007
YHWH <dreymentov@mail.ru>
2023-04-06 22:51:30 +0300

    v0.1.2 fix ontriggerenter
    when a gameobject w/t child has 2+ colliders can invoke 3+ times...

--- Assets/Scripts/Scripts_Blocks/Grass/Grass.cs
+++ Assets/Scripts/Scripts_Blocks/Grass/Grass.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Grass : MonoBehaviour
 {
-    List<Collider2D> BlocksAround = new List<Collider2D>();
+    public List<Collider2D> BlocksAround = new List<Collider2D>();
+    public int SpriteID = 9;
+    public Sprite[] sprite;
 
     // Start is called before the first frame update
     void Start()
     {
-        CheckingAroundBlock();
+
     }
 
     // Update is called once per frame
@@ -22,10 +24,11 @@ public class Grass : MonoBehaviour
     {
         if (Block.tag == "Grass")
         {
-            CheckingAroundBlock();
             Debug.Log("Enter");
             BlocksAround.Add(Block);
+            Debug.Log(this.gameObject.name);
             Debug.Log("Присобачил Блок " + (this.gameObject.transform.position - Block.transform.position));
+            CheckAround();
         }
     }
 
@@ -34,15 +37,27 @@ public class Grass : MonoBehaviour
 
         if (Block.tag == "Grass")
         {
-            CheckingAroundBlock();
             Debug.Log("Exit");
             BlocksAround.Remove(Block);
+            Debug.Log(this.gameObject.name);
             Debug.Log("Удален Блок " + (this.gameObject.transform.position - Block.transform.position));
+            CheckAround();
         }
     }
-
-    void CheckingAroundBlock()
+    
+    private void CheckAround()
     {
-        Debug.Log("Try checking...");
+        if(BlocksAround.Count > 0)
+        {
+            for (int i = 0; i < BlocksAround.Count; i++)
+            {
+                Debug.Log(this.gameObject.name);
+                Debug.Log("Коорд блока[" + i + "] " + (this.gameObject.transform.position - BlocksAround[i].gameObject.transform.position));
+            }
+        }
+        else
+        {
+            SpriteID = 0;
+        }
     }
 }
